7a9834ddf2330dd1bca53e7580916737fda85c16
nishitanand <nishitanand99@hotmail.com>
2020-08-25 23:51:41 +0530

    More Codeforces Contest Solutions

---
+++ Codeforces Round 632 Div 2/a.cpp
@@ -0,0 +1,90 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define lli long long int
+
+int main()
+{
+    lli t,n,m,i,j;
+    int flag;
+    cin>>t;
+    while(t--)
+    {
+        cin>>n>>m;
+        if ((n%2!=0)&&(m%2!=0))
+        {
+            cout<<" CASE 1 ODD ";
+            for (i=0;i<n;i++)
+            {
+                cout<<" I "<<i<<" ";
+                if (i%2==0)
+                    flag=0;
+                else
+                    flag=1;
+                for (j=0;j<m;j++)
+                {
+                    cout<<" J "<<j<<" ";
+                    if (flag==0)
+                    {
+                        cout<<"B";
+                        flag=1;
+                    }
+                    if (flag==1)
+                    {
+                        cout<<"W";
+                        flag=0;
+                    }
+                }
+                cout<<"\n";
+            }
+        }
+        else
+        {
+            cout<<" CASE 2 EVEN ";
+            for (i=0;i<n-1;i++)
+            {
+                cout<<" I "<<i<<" ";
+                if (i%2==0)
+                    flag=0;
+                else
+                    flag=1;
+                for (j=0;j<m;j++)
+                {
+                    cout<<" J "<<j<<" ";
+                    if (flag==0)
+                    {
+                        cout<<"B";
+                        flag=1;
+                    }
+                    if (flag==1)
+                    {
+                        cout<<"W";
+                        flag=0;
+                    }
+                }
+                cout<<"\n";
+            }
+            flag=0;
+            for (j=0;j<m-2;j++)
+            {
+                cout<<" J "<<j<<" ";
+                if (flag==0)
+                    {
+                        cout<<"B";
+                        flag=1;
+                    }
+                if (flag==1)
+                    {
+                        cout<<"W";
+                        flag=0;
+                    }
+            }
+            cout<<"BB\n";
+        }
+    }
+
+
+
+
+    return 0;
+}
